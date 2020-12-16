5cf9830855513da71f9fae4becb2878367d31d54
Conner Ward <ckward@uci.edu>
2020-12-15 18:55:56 -0800

    updated with label generation

--- API/app/main.py
+++ API/app/main.py
@@ -8,28 +8,82 @@ from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from hashlib import blake2b
 from pydantic import BaseModel, Field, validator
 from typing import List, Optional
-
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+import treepoem
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 import csv
 import os
+import uvicorn
 
-
+# ---------------------------------------------------------------------
+""" DELETE!!!!!!!!!!!!!!"""
+"""
+a5user
+Soeba123
+Key: 652ba85baf9928aa
+Database information 
+"""
+os.environ["DATABASE_URL"] = "stargods.net"
+os.environ["DATABASE_PORT"] = "43751"
+os.environ["DATABASE_NAME"] = "codesdb"
+os.environ["API_USERNAME"] = "admin"
+os.environ["API_PASSWORD"] = "dingo"
+# ---------------------------------------------------------------------
 app = FastAPI()
 security = HTTPBasic()
-
 api_admin_username = os.environ['API_USERNAME']
 api_admin_password = os.environ['API_PASSWORD']
-
 client = MongoClient(os.environ['DATABASE_URL'], int(os.environ['DATABASE_PORT'])) # https://stargods.net:43751/
 database_name = os.environ['DATABASE_NAME'] # codesdb
 db = client[database_name]
 collection_post_schema = "-assets"
 api_key_header = APIKeyHeader(name="X-API-KEY", auto_error=False)
+# ---------------------------------------------------------------------
+HALF_SIZE = False
 
 
 class EditableAsset(BaseModel):
     contents: list = Field(default=[], example=[])
     notes: str = Field(default="", example="")
     inuse = False
+
+origins = [
+    "http://localhost",
+    "http://localhost:4200/",
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+def gen_label(entry_dict: dict):
+    if HALF_SIZE:
+        label_size = (160, 200)  # 450, 200
+    else:
+        label_size = (425, 200)
+    im = Image.new('RGB', label_size, (0, 0, 0))
+    draw = ImageDraw.Draw(im)
+    result_img = treepoem.generate_barcode("datamatrix", entry_dict["code"], options={"version": "12x12"})
+    result_img = ImageOps.invert(result_img).convert('1')  # invert
+    result_img = ImageOps.crop(result_img, 1)
+    result_img = result_img.resize((100, 100))
+    Image.Image.paste(im, result_img, (30, 25))
+    font = ImageFont.truetype(r'Futura.ttc', 40)
+    font2 = ImageFont.truetype(r'Futura.ttc', 35)
+    efon_font = ImageFont.truetype(r'Efon.ttf', 120)
+    draw.text((31, 130), entry_dict["code"], font=font)
+    draw.text((160, 15), entry_dict["name"], font=font2)
+    draw.text((200, 75), entry_dict["namecode"], font=efon_font)
+    filename = f'generated-labels/{entry_dict["code"]}.png'
+    im.save(filename, quality=100)
+    return filename
 
 def get_apikey(username, password):
     h = blake2b(key=bytes(password.encode("UTF-8")), digest_size=8)
@@ -61,6 +115,7 @@ def parse_headers_userdata(header):
 def parse_headers_apikey(header):
     return parse_headers(header)['X-API-KEY']
 
+
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
     correct_username = secrets.compare_digest(credentials.username, api_admin_username)
     correct_password = secrets.compare_digest(credentials.password, api_admin_password)
@@ -71,6 +126,7 @@ def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
             headers={"WWW-Authenticate": "Basic"},
         )
     return credentials.username
+
 
 # ----------GENERAL------------------
 @app.get("/", include_in_schema=False)
@@ -92,6 +148,37 @@ def get_item(item_code: str, api_key: Optional[str] = Security(api_key_header)):
     ret = collection.find_one({"code": item_code})
     ret.pop("_id")
     return ret
+
+
+# GET /api/code {json} # "Get Existing Box Label Photo"
+@app.get("/api/{item_code}/label-photo")
+def get_item(item_code: str, api_key: Optional[str] = Security(api_key_header)):
+    """
+    Returns a JSON representing the given user via the provided ID.
+    Does not return the _id ObjectID, as this is for internal mongo purposes.
+    - param item_code: item code
+    - return: JSON string
+    """
+    collection = db[api_key]
+    ret = collection.find_one({"code": item_code})
+    ret.pop("_id")
+    label_directory = gen_label(ret)
+    return FileResponse(label_directory)
+
+
+# GET /api/next # "Get Next Available Box"
+@app.get("/api/next/")
+def get_next_free_item(api_key: Optional[str] = Security(api_key_header)):
+    """
+    Returns a JSON representing the given user via the provided ID.
+    Does not return the _id ObjectID, as this is for internal mongo purposes.
+    - return: JSON string
+    """
+    collection = db[api_key]
+    ret = collection.find_one({"inuse": False})
+    ret.pop("_id")
+    return ret
+
 
 # GET /api/?query&query {json} # "Get Query"
 @app.get("/api/search/")
@@ -118,6 +205,7 @@ def query_item(limit: int = 10,
     ret = [elem for elem in collection.find(query, limit=limit)]
     [elem.pop("_id") for elem in ret]
     return ret
+
 
 # PUT /api/code {json} # "Enable New Box, Set Notes"
 @app.put("/api/{item_code}")
@@ -212,3 +300,7 @@ def delete_user(response: Response, username: Optional[str] = Header(None), pass
     print(db.command({"dropRole": f'{get_apikey(username=username, password=password)}_CollectionRole'}))
     print(db.command({"dropUser": username}))
     return response
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", log_level="info")
